aa7ba4ee86ce588374a22c60dbb671557adb4f00
Z.R. ZHAO <garry@azurflow.com>
2024-09-25 23:33:45 +0800

    FEAT: add base ManualExecutor impl

--- executors/__init__.py
+++ executors/__init__.py
@@ -1,8 +1,10 @@
 from .executor import Executor
 from .passive_executor import PassiveExecutor
+from .manual_executor import ManualExecutor
 
 
 __all__ = [
     'Executor',
     'PassiveExecutor',
+    'ManualExecutor',
 ]
