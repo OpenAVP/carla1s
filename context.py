5b4f679f488d98d8988b4b17ffd5525852f74a8d
Z.R. ZHAO <garry@azurflow.com>
2024-09-25 08:31:26 +0800

    DOCS: update code comments

--- context.py
+++ context.py
@@ -5,16 +5,10 @@ from rich.logging import RichHandler
 
 
 class Context:
-    """CARLA1S 的上下文聚合, 管理 CARLA 任务的生命周期.
-    
-    强烈建议使用 with 语句来创建上下文, 以确保资源的正确释放.
-    
-    Example:
-    ```python
-        with Context() as ctx:
-            ctx.do()
-    ```
-    
+    """
+    CARLA1S 的上下文聚合, 管理 CARLA 任务的生命周期.
+
+    该类实现了上下文管理器协议, 可以使用 with 语句来管理 CARLA 任务的生命周期.
     """
     
     def __init__(self,
@@ -24,12 +18,11 @@ class Context:
                  log_level: int = logging.INFO,
                  logger: Optional[logging.Logger] = None) -> None:
         """
-        Args:
-            host (str, optional): CARLA 服务端的 IP 地址. 默认为 '127.0.0.1'.
-            port (int, optional): CARLA 服务端的端口号. 默认为 2000.
-            timeout_sec (float, optional): CARLA 服务端的超时秒数. 默认为 1.0.
-            log_level (int, optional): 日志记录器的日志级别. 默认为 logging.INFO.
-            logger (Optional[logging.Logger], optional): 自定义的日志记录器实例. 为 None 时默认创建一个.
+        :param host: CARLA 服务端的 IP 地址.
+        :param port: CARLA 服务端的端口号.
+        :param timeout_sec: CARLA 客户端的超时时间.
+        :param log_level: 日志记录的级别.
+        :param logger: 用于记录日志的 Logger 实例, 如果为 None, 则会创建一个新的 RichLogger 实例.
         """
         self._host = host
         self._port = port
@@ -53,16 +46,23 @@ class Context:
     
     @property
     def client(self) -> carla.Client:
-        """返回 CARLA 客户端实例."""
+        """
+        :return: 当前上下文中的 CARLA 客户端实例.
+        """
         return self._client
     
     @property
     def actor(self) -> list:
-        """返回当前上下文中的 Actor 列表."""
+        """
+        :return: 当前上下文中的所有 Actor 实例.
+        """
         return self._actors
             
     def _create_logger(self) -> logging.Logger:
-        """创建一个用于记录日志的 Logger 实例."""
+        """
+        创建一个默认的 logging.Logger 实例, 并应用 RichHandler.
+        :return: logging.Logger 实例.
+        """
         logger = logging.getLogger('carla1s')
         logger.setLevel(self._log_level)
         handler = RichHandler(rich_tracebacks=True)
@@ -70,7 +70,10 @@ class Context:
         return logger
     
     def connect(self) -> 'Context':
-        """连接到 CARLA 服务端."""
+        """
+        执行与 CARLA 服务端的连接.
+        :return: 链式调用.
+        """
         # 构造 CARLA 客户端实例
         self._client = carla.Client(host=self._host, port=self._port)
         self._client.set_timeout(self._timeout_sec)
@@ -79,10 +82,16 @@ class Context:
         self._client.get_server_version()
         
     def disconnect(self) -> 'Context':
-        """断开与 CARLA 服务端的连接."""
+        """
+        断开与 CARLA 服务端的连接.
+        :return: 链式调用.
+        """
         self._client = None
         
     def destroy_all_actors(self) -> None:
-        """使用 CARLA 批处理 API 销毁所有注册在 Context 下的 Actor."""
+        """
+        销毁当前上下文中的所有 Actor 实例.
+        :return: None.
+        """
         cmd = [carla.command.DestroyActor(x) for x in self.__actor_registry]
         self.client.apply_batch_sync(cmd)
