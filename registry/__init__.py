0e20d973c9bd9050d35bd3bc20ac0edbfa6dde7b
Z.R. ZHAO <garry@azurflow.com>
2024-09-28 15:08:14 +0800

    FEAT: add registry template

---
+++ registry/__init__.py
@@ -0,0 +1,12 @@
+from .available_maps import AvailableMaps
+from .available_actors import AvailableActors
+from .available_sensors import AvailableSensors
+from .available_vehicles import AvailableVehicles
+
+
+__all__ = [
+    'AvailableMaps',
+    'AvailableActors',
+    'AvailableSensors',
+    'AvailableVehicles',
+]
