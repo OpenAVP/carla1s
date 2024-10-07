4616076102a442ae9203f91b6d9e17583dac722b
Z.R. ZHAO <garry@azurflow.com>
2024-10-07 23:46:16 +0800

    FIX: duplicate handler

--- utils/logging/get_logger.py
+++ utils/logging/get_logger.py
@@ -8,5 +8,6 @@ def get_logger(name: str) -> logging.Logger:
     logger = logging.getLogger(name)
     handler = RichHandler(rich_tracebacks=True, markup=False)
     handler.setFormatter(ProjectFormatter('[%(shortname)s] %(message)s'))
+    logger.handlers.clear()
     logger.addHandler(handler)
     return logger
