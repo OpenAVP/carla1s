3947e4604c58da4049185bbea70ca99536ef6172
Z.R. ZHAO <garry@azurflow.com>
2024-10-08 10:57:20 +0800

    FEAT: clear on_data_ready event when new callback incoming

--- actors/rgb_camera.py
+++ actors/rgb_camera.py
@@ -6,6 +6,7 @@ from .sensor import Sensor
 class RgbCamera(Sensor):
     
     def _callback(self, image: carla.Image):
+        self.on_data_ready.clear()
         img = np.frombuffer(image.raw_data, dtype=np.uint8)
         img = img.reshape(
             (image.height, image.width, img.shape[0] // image.height // image.width)
