a97bfeef0ebf2cf1208a9b48a58e35cfe8169484
isleep <jessewang59@gmail.com>
2024-10-18 22:51:20 +0800

    FEAT: add coordinate in __init__

--- tf/__init__.py
+++ tf/__init__.py
@@ -1,10 +1,12 @@
 from .transform import Transform
 from .point import Point
 from .coord_converter import CoordConverter
+from .coordinate import Coordinate
 
 
 __all__ = [
     "Transform",
     "Point",
     "CoordConverter",
+    "Coordinate"
 ]
