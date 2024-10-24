6f5accafa14ffd95b3a910cefb87f375f9f9a56d
suayu <suayuzyx@163.com>
2024-10-24 19:27:31 +0800

    FEAT:add save control path,follow lines control and draw lines control

---
+++ utils/waypoint/follow_waypoint_control.py
@@ -0,0 +1,143 @@
+import math
+import time
+import random
+import carla
+import os
+from .get_tf import interpolate, read_data
+import numpy as np
+from .runtime import runtime
+from .PurePursuit import SimpleTest
+
+# 参考点最大间距
+THRESHOLD = 1
+# 参考点期望间距
+TARGET_DIS = 0.1
+
+def calculate_yaws(x, y):
+    yaws = []
+    for i in range(len(x) - 1): 
+        dx = x[i + 1] - x[i]
+        dy = y[i + 1] - y[i]
+        yaw = math.atan2(dx, dy)
+        yaw = math.degrees(yaw)
+        if yaw > 90:
+            yaw = - (yaw - 90)
+        else:
+            if -90 <= yaw <= 90:
+                yaw = 90 - yaw
+            else:
+                yaw = - yaw - 270
+        yaws.append(yaw)
+    return yaws 
+
+def save_track(new_path, save_path):
+    with open(save_path, 'w') as file:
+        for waypoint in new_path: 
+            file.write(f"{waypoint[0]}, {waypoint[1]}, {np.degrees(waypoint[2])}\n")
+    print(f"The driving path has been saved to {save_path}")
+
+def follow_track(data_path = "tf.npy", forward = True, create_ego_car = True, reference_points = [], map_name = '', final_track_save_path = ''):
+    dT = 0.02
+
+    if not len(reference_points) == 0:
+
+        # 有轨迹输入，采用轨迹输入
+        temp_x = np.array([point[0] for point in reference_points])
+        temp_y = np.array([point[1] for point in reference_points])
+        z = np.array([0])
+
+        # 考虑到参考路径点可能过于稀疏，进行上采样，以方便PurePursuit算法追踪
+        path_length = 0
+        x_ = []
+        y_ = []
+        for i in range(0,len(temp_x)-1):
+            dis = math.sqrt((temp_x[i]-temp_x[i+1])*(temp_x[i]-temp_x[i+1])+(temp_y[i]-temp_y[i+1])*(temp_y[i]-temp_y[i+1]))
+            path_length += dis
+            if dis > THRESHOLD:
+                target_dis = TARGET_DIS
+                target_points_num = int(dis / target_dis) + 1
+
+                xp = np.array([temp_x[i],temp_x[i+1]])
+                yp = np.array([temp_y[i],temp_y[i+1]])
+
+                xn = np.linspace(temp_x[i], temp_x[i+1], target_points_num)
+                yn = np.interp(xn, xp, yp)
+
+                x_.extend(xn.tolist())
+                y_.extend(yn.tolist())
+            else:
+                x_.append(temp_x[i])
+                y_.append(temp_y[i])
+
+        new_len = len(x_)
+        x = np.array(x_)
+        y = np.array(y_)
+        yaw = calculate_yaws(x,y)
+
+    elif len(reference_points) == 0 and os.path.exists(data_path):
+
+        # 无轨迹输入，且文件存在，采用文件数据
+        transform, time_stamp = read_data(data_path)
+
+        # 使用插值法修正轨迹
+        x, y, z, pitch, yaw, roll = interpolate(transform, time_stamp, dT)
+        new_len = x.shape[0]
+
+        # 轨迹总长度
+        path_length = 0
+        for i in range(0,new_len-1):
+            path_length += math.sqrt((x[i]-x[i+1])*(x[i]-x[i+1])+(y[i]-y[i+1])*(y[i]-y[i+1]))
+    else:
+
+        # 无轨迹输入，且文件不存在，报错
+        print("Failed to find reference path!")
+        return
+
+    # 使用PurePursuit算法修正轨迹
+    path = SimpleTest.get_path(x,y)
+    # 前进把速度设成1，倒车把速度设成-1
+    if forward == True:
+        v = 1
+    else:
+        v = -1
+    state = np.array([x[0], y[0], np.radians(yaw[0]), v]).astype(np.float64)
+    test = SimpleTest(initial_state = state)
+    new_path = test.offline_test(path, math.floor(path_length/(v*runtime['dt']))-1)
+
+    if not len(final_track_save_path) == 0:
+        save_track(new_path, final_track_save_path)
+
+    x = [state[0] for state in new_path]
+    y = [state[1] for state in new_path]
+    yaw = [np.degrees(state[2]) for state in new_path]
+    new_len = len(new_path)
+
+    client = carla.Client('localhost', 2000)
+    if not len(map_name) == 0:
+        client.load_world(map_name)
+    world = client.get_world()
+    if create_ego_car == True:
+        spawn_point = random.choice(world.get_map().get_spawn_points())
+        vehicle_bp = world.get_blueprint_library().filter('*vehicle*').filter('vehicle.tesla.*')[0]
+        ego_vehicle = world.spawn_actor(vehicle_bp, spawn_point)
+    else:
+        ego_vehicle = world.get_actors().filter('vehicle.*')[0]
+    cur_location = carla.Location(x = x[0], y = y[0], z = z[0]-0.0035)
+
+    world.get_spectator().set_transform(carla.Transform(cur_location+carla.Location(z=3),carla.Rotation(pitch=0)))
+
+    for i in range(1,new_len):
+        location = carla.Location(x = x[i], y = y[i], z = z[0]-0.0035)
+        ego_vehicle.set_transform(carla.Transform(location,carla.Rotation(pitch=0,yaw=yaw[i],roll=0)))
+
+        velocity = carla.Vector3D(x = (x[i] - cur_location.x) / dT, y = (y[i] - cur_location.y) / dT, z = 0)
+        v = math.sqrt(velocity.x * velocity.x + velocity.y*velocity.y + velocity.z*velocity.z)
+
+        world.debug.draw_line(cur_location + carla.Location(z = runtime['H'] / 2), location + carla.Location(z = runtime['H'] / 2), thickness=0.1, color=carla.Color(r = 0, g = 0, b = 255), life_time = 10)
+        cur_location = location
+        time.sleep(dT)
+
+if __name__ == "__main__":
+    follow_track() 
+
+
--- utils/waypoint/waypoints.py
+++ utils/waypoint/waypoints.py
@@ -1,8 +1,10 @@
 import numpy as np
+import math
 from typing import List, Union
-
+from .follow_waypoint_control import calculate_yaws
+from .get_tf import interpolate, read_data
+from .PurePursuit import SimpleTest
 from ...tf import Transform
-
 
 class Waypoints:
     def __init__(self, 
@@ -12,18 +14,32 @@ class Waypoints:
                  forward: bool = True,
                  keep_last: bool = False):
         self._delta_seconds = delta_seconds
-        self._sequence = self._smoothing(sequence)
         self._iter_index = 0
+        self._path_length = 0
         self.keep_last = keep_last
         self.forward = forward
+        self._threshold = 1
+        self._target_dis = 0.2
+        self._ori_yaw = 0
+        self._sequence = self._smoothing(sequence)
         
     def __len__(self):
         # TODO: CONFIRM IT!
-        return len(self._sequence)
+        if isinstance(self._sequence, list):
+            return len(self._sequence)
+        elif isinstance(self._sequence, np.array):
+            return self._sequence.shape[0]
     
     def __getitem__(self, index: int):
         # TODO: CONFIRM IT!
-        return self._sequence[index]
+
+        #return self._sequence[index]
+        item = self._sequence[index]
+        if isinstance(item, Transform):
+            return item
+        elif isinstance(item, np.array):
+            print("type(item):",type(item))
+            return Transform(x=item[0], y=item[1], z=item[2], pitch=item[3], yaw=item[4], roll=item[5])
     
     def __iter__(self):
         return self
@@ -38,10 +54,12 @@ class Waypoints:
 
         waypoint = self._sequence[self._iter_index]
         self._iter_index += 1
-        
-        # TODO: Convert to Transform Object
-        
-        return waypoint
+
+        if isinstance(waypoint, Transform):
+            return waypoint
+        elif isinstance(waypoint, np.array):
+            waypoint = Transform(x=waypoint[0], y=waypoint[1], z=waypoint[2], pitch=waypoint[3], yaw=waypoint[4], roll=waypoint[5])
+            return waypoint
 
     @property
     def data(self):
@@ -54,15 +72,78 @@ class Waypoints:
         return next(self)
 
     def _smoothing(self, sequence: np.ndarray) -> np.ndarray:
-        sequence = self._up_sampling(sequence)
-        sequence = self._pure_pursuit(sequence)
+        if isinstance(sequence, list):
+            transform_list = [[sequence[i].x, sequence[i].y, sequence[i].z, sequence[i].pitch, sequence[i].yaw, sequence[i].roll, 0, i * self._delta_seconds, 0, 0] for i in range(0,len(sequence))]
+            sequence = np.array(transform_list)
+            sequence = self._up_sampling(sequence)
+            sequence = self._pure_pursuit(sequence)
+        elif isinstance(sequence, np.array):
+
+            # 输入的sequence格式：x y z pitch yaw roll frame elapsed_seconds delta_seconds platform_timestamp
+            sequence = self._up_sampling(sequence)
+            sequence = self._pure_pursuit(sequence)
         return sequence
     
-    def _up_sampling(self, sequence: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
+    def _up_sampling(self, sequence: np.ndarray, save = False) -> np.ndarray:
+        transform = sequence[:,:6]
+        time_stamp = sequence[:,-4:]
+        temp_x, temp_y, z, pitch, yaw, roll = interpolate(transform, time_stamp, self._delta_seconds)
+
+        # 考虑到参考路径点可能过于稀疏，进行上采样，以方便PurePursuit算法追踪
+        path_length = 0
+        x_ = []
+        y_ = []
+        for i in range(0,len(temp_x)-1):
+            dis = math.sqrt((temp_x[i]-temp_x[i+1])*(temp_x[i]-temp_x[i+1])+(temp_y[i]-temp_y[i+1])*(temp_y[i]-temp_y[i+1]))
+            path_length += dis
+            if dis > self._threshold:
+                target_dis = self._target_dis
+                target_points_num = int(dis / target_dis) + 1
+
+                xp = np.array([temp_x[i],temp_x[i+1]])
+                yp = np.array([temp_y[i],temp_y[i+1]])
+
+                xn = np.linspace(temp_x[i], temp_x[i+1], target_points_num)
+                yn = np.interp(xn, xp, yp)
+
+                x_.extend(xn.tolist())
+                y_.extend(yn.tolist())
+            else:
+                x_.append(temp_x[i])
+                y_.append(temp_y[i])
+
+        x = np.array(x_)
+        y = np.array(y_)
+        self._path_length = path_length
+
+        yaw = calculate_yaws(x,y)
+        self._ori_yaw = yaw[0]
+        if save == True:
+            self._sequence = [Transform(x=x, y=y, z=0, pitch=0, yaw=yaw, roll=0) for x, y, yaw in zip(x, y, yaw)]
+        return np.concatenate((x.reshape(-1,1), y.reshape(-1,1)), axis = 1)
     
-    def _pure_pursuit(self, sequence: np.ndarray) -> np.ndarray:
-        raise NotImplementedError
+    def _pure_pursuit(self, sequence: np.ndarray, forward = True, save = False) -> np.ndarray:
+        # 使用PurePursuit算法修正轨迹
+
+        # 前进把速度设成1，倒车把速度设成-1
+        if forward == True:
+            v = 1
+        else:
+            v = -1
+        state = np.array([sequence[0][0], sequence[0][1], np.radians(self._ori_yaw), v]).astype(np.float64)
+        test = SimpleTest(initial_state = state)
+        new_path = test.offline_test(sequence, math.floor(self._path_length/(v*self._delta_seconds))-1)
+
+        x = [state[0] for state in new_path]
+        y = [state[1] for state in new_path]
+        yaw = [np.degrees(state[2]) for state in new_path]
+        new_len = len(new_path)
+
+        sequence = [Transform(x=x, y=y, z=0, pitch=0, yaw=yaw, roll=0) for x, y, yaw in zip(x, y, yaw)]
+        if save == True:
+            self._sequence = sequence
+        return sequence
+        # raise NotImplementedError
 
     @classmethod
     def from_file(cls, 
@@ -71,4 +152,10 @@ class Waypoints:
                   delta_seconds: float, 
                   forward: bool = True, 
                   keep_last: bool = False) -> 'Waypoints':
-        raise NotImplementedError
+        transform, time_stamp = read_data(file_path)
+        x, y, z, pitch, yaw, roll = interpolate(transform, time_stamp, delta_seconds)
+        new_len = x.shape[0]
+        sequence = [Transform(x=x, y=y, z=z, pitch=pitch, yaw=yaw, roll=roll) for x, y, z, pitch, yaw, roll in zip(x, y, z, pitch, yaw, roll)]
+        waypoints = Waypoints(sequence = sequence, delta_seconds = delta_seconds)
+        return waypoints
+        # raise NotImplementedError
